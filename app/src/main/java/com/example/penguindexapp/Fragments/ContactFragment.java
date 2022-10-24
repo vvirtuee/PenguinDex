0952acba00d5d429270d8c004b6d7994a36ecbd2
Dalton Virtue <w0806469@myscc.ca>
2022-10-24 14:39:55 -0400

    #2 Added web intent specifically social media connection to Instagram

---
+++ app/src/main/java/com/example/penguindexapp/Fragments/ContactFragment.java
@@ -0,0 +1,119 @@
+package com.example.penguindexapp.Fragments;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.penguindexapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ContactFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ContactFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ContactFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ContactFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ContactFragment newInstance(String param1, String param2) {
+        ContactFragment fragment = new ContactFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.fragment_contact, container, false);
+        Button emailBtn = v.findViewById(R.id.emailBtn);
+        emailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] emailAddress = {"w0806469@myscc.ca"};
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                i.setData(Uri.parse("mailto:"));
+                i.putExtra(Intent.EXTRA_EMAIL, emailAddress);
+                i.putExtra(Intent.EXTRA_SUBJECT, "PenguinDex Tech Support Team");
+                i.putExtra(Intent.EXTRA_CC, "techsupport@penguindex.com");
+                i.putExtra(Intent.EXTRA_TEXT, "");
+
+                startActivity(i);
+            }
+        });
+
+        Button phoneBtn = v.findViewById(R.id.phoneBtn);
+        phoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                i.setData(Uri.parse("smsto:5199722727"));
+                i.putExtra("sms_body","I am sending you this message regarding the following...");
+                startActivity(i);
+            }
+        });
+
+        Button mapsBtn = v.findViewById(R.id.mapsBtn);
+        mapsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri geoLocation = Uri.parse("geo:-62.1014, -57.9296");
+                Intent intent = new Intent(Intent.ACTION_VIEW, geoLocation);
+                intent.setData(geoLocation);
+                startActivity(intent);
+            }
+        });
+        Button websiteBtn = v.findViewById(R.id.websiteBtn);
+        websiteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.instagram.com/penguindex";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        return v;
+    }
+}

2ec0532ef3bd09fd3cc1092efd1d65cdacc9e7b4
Dalton Virtue <w0806469@myscc.ca>
2022-11-29 16:25:42 -0500

    @cfiliault having an issue with drawer navigation after implementing settings. my navController defined in MainActivity wont work properly once I added the code on line 61.

--- app/src/main/java/com/example/penguindexapp/MainActivity.java
+++ app/src/main/java/com/example/penguindexapp/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.penguindexapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +51,16 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_credits)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        navController.navigate(R.id.nav_settings);
+        //System.out.println("item selected: " + item);
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
